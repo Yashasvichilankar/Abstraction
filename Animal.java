8c3c6629d55323bc7dd2444a402c84ec58abe367
Yashasvi <yashasvichilankar27@gmail.com>
2023-04-04 16:11:36 +0530

    Adding file

---
+++ Animal.java
@@ -0,0 +1,36 @@
+abstract class Animals
+{
+	abstract void cats();
+	abstract void dogs();
+}
+class Cats extends Animals
+{
+	@Override
+	void cats() {
+		// TODO Auto-generated method stub
+		System.out.println("Cats meow meow");
+	}
+	@Override
+	void dogs() {
+		// TODO Auto-generated method stub	
+	}
+}
+class Dogs extends Animals
+{
+	@Override
+	void cats() {
+		// TODO Auto-generated method stub	
+	}
+	@Override
+	void dogs() {
+		System.out.println("Dogs bhaoo bhaoooo");
+	}
+}
+public class Animal {
+	public static void main(String[] args) {
+		Cats c=new Cats();
+		c.cats();
+		Dogs d=new Dogs();
+		d.dogs();
+	}
+}
